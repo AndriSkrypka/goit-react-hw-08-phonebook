4d7f0d1f20f8a2317559f8ac2177648938065144
AndriSkrypka <102464331+AndriSkrypka@users.noreply.github.com>
2023-06-27 23:56:25 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>  
       <PersistGate loading={null} persistor={persistor}>
-         <BrowserRouter  basename="/home/runner/work/goit-react-hw-08-phonebook/goit-react-hw-08-phonebook/src/redux/">
+         <BrowserRouter  basename="/goit-react-hw-08-phonebook/">
             <App />
          </BrowserRouter>  
       </PersistGate>     
